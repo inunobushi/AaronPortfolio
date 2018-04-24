1669222e11beadcb89120b23e63a94de67430326
inunotaisho26 <ebfairweather@gmail.com>
2018-04-23 22:04:12 -0400

    reverting copyright component

--- AaronPortfolio/ClientApp/Components/CopyrightComponent/CopyrightComponent.tsx
+++ AaronPortfolio/ClientApp/Components/CopyrightComponent/CopyrightComponent.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import * as Redux from 'redux';
 import * as ReactRedux from 'react-redux';
-import {AboutComponent, Page} from '../AboutComponent/AboutComponent';
+import {AboutComponent} from '../AboutComponent/AboutComponent';
 
 export class CopyRightComponent extends React.Component{
-
-    page:Page = new Page();
     render() {
         return(
             <div className="copyright-details">
                 <div className="copyright">
-                    <div className= {{Page.class1},"text-center"}>
-                        <ul className={{Page.class2},"about-social"}>
+                    <div className= 'about-social-icon text-center'>
+                        <ul className="about-social">
                             <li className="wow fadeIn" data-wow-delay=".2s"><a href="www.facebook.com" target="newtab"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
                             <li className="wow fadeIn" data-wow-delay=".4s"><a href="www.twitter.com" target="newtab"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
                             <li className="wow fadeIn" data-wow-delay=".6s"><a href="www.pinterest.com" target="newtab"><i className="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
