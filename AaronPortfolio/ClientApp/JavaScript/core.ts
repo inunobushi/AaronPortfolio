92e1c86110750e0bf9eafc50c549e58103cf93e0
inunotaisho26 <ebfairweather@gmail.com>
2018-05-13 10:26:16 -0400

    adding core.ts

---
+++ AaronPortfolio/ClientApp/JavaScript/core.ts
@@ -0,0 +1,6 @@
+import '../JavaScript/custom.js';
+import '../JavaScript/jquery.fancybox-buttons.js';
+import '../JavaScript/jquery.fancybox-media.js';
+import '../JavaScript/jquery.fancybox-thumbs.js';
+import '../JavaScript/jquery.fancybox.js';
+import '../JavaScript/jquery.fancybox.pack.js';
