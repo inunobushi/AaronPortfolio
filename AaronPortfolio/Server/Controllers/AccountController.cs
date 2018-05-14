131ea5dc7ae690b900fc43a9eb4124583a8c19eb
inunotaisho26 <ebfairweather@gmail.com>
2018-05-14 18:15:51 -0400

    creating accounts controller

--- AaronPortfolio/Server/Controllers/AccountController.cs
+++ AaronPortfolio/Server/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AaronPortfolio.Server.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,36 +12,17 @@ namespace AaronPortfolio.Server.Controllers
     [Route("api/[controller]")]
     public class AccountController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        
+        [HttpPost("register")]
+		public void Register([FromBody]UserViewModel user)
+		{
+			var newUser  = user;
+		}
+        
+		[HttpPost("login")]
+        public void Login([FromBody]LoginViewModel user)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var newUser = user;
         }
     }
 }
