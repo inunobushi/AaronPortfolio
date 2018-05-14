edd175afc5b8e34f1cd46eb387adc0f6578498d2
inunotaisho26 <ebfairweather@gmail.com>
2018-05-14 11:13:07 -0400

    adding mongodb

---
+++ AaronPortfolio/Server/Models/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Scheduling.AaronPortfolio.Server.Models
+{
+    public class Settings
+    {
+		public string ConnectionString;
+
+        public string Database;
+    }
+}
