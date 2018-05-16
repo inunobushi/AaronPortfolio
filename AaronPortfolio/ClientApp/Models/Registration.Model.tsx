82211849e260127bdbd2f5eca9779b75bc95beca
inunotaisho26 <ebfairweather@gmail.com>
2018-05-16 10:42:18 -0400

    adding constructor, removing username field and changing model name

--- AaronPortfolio/ClientApp/Models/Registration.Model.tsx
+++ AaronPortfolio/ClientApp/Models/Registration.Model.tsx
@@ -1,7 +1,19 @@
-export class IRegistrationModel {
-    Username: string;
+export class RegistrationModel {
+    constructor(
+        FirstName: string,
+        LastName: string,
+        Email:string,
+        Password:string,
+        PasswordConfirm: string){
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Password = Password;
+            this.PasswordConfirm = PasswordConfirm;
+    }
     FirstName: string;
     LastName: string;
     Email:string;
     Password:string;
+    PasswordConfirm: string;  
 }
