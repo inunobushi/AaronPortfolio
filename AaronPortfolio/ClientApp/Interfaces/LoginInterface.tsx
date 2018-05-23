066f194f57dcb05860eb4ec16d46dbf630219e56
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 16:03:16 -0400

    Creating Interfaces folder

---
+++ AaronPortfolio/ClientApp/Interfaces/LoginInterface.tsx
@@ -0,0 +1,9 @@
+export interface ILoginFields {
+    FirstName: string,
+    LastName: string,
+    Email: string,
+    Password: string,
+    PasswordConfirm: string,
+    updateFieldAuth: any,
+    submitForm: any
+  }
---
+++ AaronPortfolio/ClientApp/Interfaces/RegInterface.tsx
@@ -0,0 +1,9 @@
+export interface IRegFields {
+    FirstName: string,
+    LastName: string,
+    Email: string,
+    Password: string,
+    PasswordConfirm: string,
+    updateFieldAuth: any,
+    submitForm: any
+  }
