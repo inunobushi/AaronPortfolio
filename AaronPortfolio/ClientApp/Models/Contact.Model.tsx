8b6889964547ae2600e26dcd420940bf949826c9
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 17:05:08 -0400

    adding constructor to Contact Model

--- AaronPortfolio/ClientApp/Models/Contact.Model.tsx
+++ AaronPortfolio/ClientApp/Models/Contact.Model.tsx
@@ -1,4 +1,19 @@
-﻿export class IContactModel {
+﻿export class ContactModel {
+
+    constructor(
+        FirstName: string,
+    LastName: string,
+    Email: string,
+    Subject: string,
+    Message: string
+    ){
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Email = Email;
+        this.Subject = Subject;
+        this.Message = Message;
+
+    }
     FirstName: string;
     LastName: string;
     Email: string;
