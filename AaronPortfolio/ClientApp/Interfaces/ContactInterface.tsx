852d22fcab9d70ff95cda038c9624430bde62c5a
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 17:04:04 -0400

    creating contact interface

---
+++ AaronPortfolio/ClientApp/Interfaces/ContactInterface.tsx
@@ -0,0 +1,9 @@
+export interface IContactFields {
+    FirstName: string,
+    LastName: string,
+    Email: string,
+    Subject: string,
+    Message: string,
+    updateFieldContact: any,
+    submitContactForm: any
+  }
