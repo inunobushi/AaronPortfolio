9cecb97600140d92455bdc2774251277d6a15fc1
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 16:03:35 -0400

    creating login api

---
+++ AaronPortfolio/ClientApp/API/LoginAPI.tsx
@@ -0,0 +1,17 @@
+import AuthService from '../Services/AuthService';
+
+const Login = (e:any) =>{
+    e.preventDefault();
+    this.AuthService.login({
+        Email:this.user.Email['value'],
+        Password:this.user.Password['value']
+    })
+    .then((data: any)=> {
+            console.log(data);
+            this.AuthService.setIsLoggedIn(data['token']);
+            window.location.pathname = '/'
+        
+    }).catch(err => {
+        console.log('err');
+    })
+}
