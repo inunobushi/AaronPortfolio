8cb6d4d07eb0e408dda5ea77a9c041af6eacb3a3
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 16:11:33 -0400

    adding constructor to Login Model

--- AaronPortfolio/ClientApp/Models/Login.Model.tsx
+++ AaronPortfolio/ClientApp/Models/Login.Model.tsx
@@ -1,4 +1,9 @@
-﻿export class ILoginModel {
+﻿export class LoginModel {
+    constructor( Email:string, Password:string)
+    {
+        this.Email = Email;
+        this.Password = Password;
+    }
     Email: string;
     Password: string;
 }
