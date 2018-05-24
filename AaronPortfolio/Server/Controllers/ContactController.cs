9d04b696fcd6342e5a96a7a67ea79215f1d026c1
inunotaisho26 <ebfairweather@gmail.com>
2018-05-24 07:49:35 -0400

    creating contact controller

---
+++ AaronPortfolio/Server/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AaronPortfolio.Server.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AaronPortfolio.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        
+        // POST api/values
+        [HttpPost("contact")]
+        public void Post([FromBody] ContactViewModel contactForm)
+        {
+            var ContactForm = contactForm;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
