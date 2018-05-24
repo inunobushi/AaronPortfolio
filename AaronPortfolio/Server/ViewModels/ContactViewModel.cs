e1d1266778fd22516b1c2259cc769a1cfbadcd04
inunotaisho26 <ebfairweather@gmail.com>
2018-05-24 07:49:46 -0400

    creating contact view model

---
+++ AaronPortfolio/Server/ViewModels/ContactViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace AaronPortfolio.Server.ViewModels
+{
+    public class ContactViewModel
+    {
+
+        public string FirstName 
+        { 
+            get; 
+            set; 
+        }
+
+        public string LastName 
+        { 
+            get; 
+            set; 
+        }
+
+        public string Email 
+        { 
+            get; 
+            set; 
+        }
+
+        public string Subject 
+        { 
+            get; 
+            set; 
+        }
+
+        public string Message 
+        { 
+            get; 
+            set; 
+        }
+
+    }
+}
