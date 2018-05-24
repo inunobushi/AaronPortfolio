d082aad26418d3378cedb6a08180a8326ce5f70c
inunotaisho26 <ebfairweather@gmail.com>
2018-05-23 22:42:41 -0400

    adding recaptcha component

---
+++ AaronPortfolio/ClientApp/Components/ReCaptcha/ReCaptcha.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {render} from "react-dom";
+import ReCAPTCHA from "react-google-recaptcha";
+const grecaptchaObject = {}
+
+class ReCaptcha extends React.Component <{}>{
+
+    constructor(props){
+        super(props);
+    }
+}
