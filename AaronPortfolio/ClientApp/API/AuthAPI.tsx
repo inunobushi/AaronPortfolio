2e1317b5ecee62d9a6fa361eb37840241564724d
inunotaisho26 <ebfairweather@gmail.com>
2018-05-25 14:44:37 -0400

    changing authapi

--- AaronPortfolio/ClientApp/API/AuthAPI.tsx
+++ AaronPortfolio/ClientApp/API/AuthAPI.tsx
@@ -1,16 +1,7 @@
 import axios from 'axios';
 
 const Registration = (user) => {
-    axios.post('api/account/register/', this.user, { headers: { 'Content-Type': 'application/json' } }).then(res => {
-        if (res.status === 200) {
-            console.log(res);
-        } else {
-            console.log(res);
-        }
-    }).catch(err => {
-        console.log(err);
-        console.log('not logging in');
-    })
+   return axios.post('api/account/register/', user, { headers: { 'Content-Type': 'application/json' } })
 }
 
-export default (Registration);
+export default {Registration};
