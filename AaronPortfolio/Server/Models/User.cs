25e97842fe41be95715ab556dc0f4c4bc68c2774
inunotaisho26 <ebfairweather@gmail.com>
2018-05-26 13:23:46 -0400

    reordering model

--- AaronPortfolio/Server/Models/User.cs
+++ AaronPortfolio/Server/Models/User.cs
@@ -14,10 +14,10 @@ namespace Scheduling.AaronPortfolio.Server.Models
         public string Id { get; set; }
 
         public string Username { get; set; }
-        
-        public string Password { get; set; }
 
         public string Email { get; set; }
+        
+        public string Password { get; set; }
 
         public string FirstName { get; set; }
 
