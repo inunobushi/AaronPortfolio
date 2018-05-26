e2f52a30c564daff69f3beec8a637a1c7b76dfd8
inunotaisho26 <ebfairweather@gmail.com>
2018-05-26 13:24:22 -0400

    updating api

--- AaronPortfolio/ClientApp/API/ContactAPI.tsx
+++ AaronPortfolio/ClientApp/API/ContactAPI.tsx
@@ -1,27 +1,8 @@
 import axios from 'axios';
 
-const Contact = (e: any, contactForm: any) => {
-
-    e.preventDefault();
-    axios.post('api/account/contact/', this.contactForm, { headers: { 'Content-Type': 'application/json' } })
-    .then((data: any) => {
-    })
+const Contact = (contactForm: any) => {
+    return axios.post('api/contact/contactdata', contactForm, { headers: { 'Content-Type': 'application/json' } })
 }
 
-export default (Contact);
+export default {Contact};
 
-// {
-//     FirstName: this.contactForm.FirstName['value'],
-//     LastName: this.contactForm.LastName['value'],
-//     Email: this.contactForm.Email['value'],
-//     Subject: this.contactForm.Subject['value'],
-//     Message: this.contactForm.Message['value']
-// }
-
-// {
-//     this.contactForm.FirstName['value'] = '';
-//     this.contactForm.LastName['value'] = '';
-//     this.contactForm.Email['value'] = '';
-//     this.contactForm.Subject['value'] = '';
-//     this.contactForm.Message['value'] = '';
-// }
--- AaronPortfolio/ClientApp/API/LoginAPI.tsx
+++ AaronPortfolio/ClientApp/API/LoginAPI.tsx
@@ -1,19 +1,8 @@
 import AuthService from '../Services/AuthService';
+const Auth = new AuthService();
 
-const Login = (e:any) =>{
-    e.preventDefault();
-    this.AuthService.login({
-        Email:this.user.Email['value'],
-        Password:this.user.Password['value']
-    })
-    .then((data: any)=> {
-            console.log(data);
-            this.AuthService.setIsLoggedIn(data['token']);
-            window.location.pathname = '/'
-        
-    }).catch(err => {
-        console.log('err');
-    })
+const Login = (user: any) =>{
+    return Auth.login(user);
 }
 
-export default (Login);
+export default {Login};
