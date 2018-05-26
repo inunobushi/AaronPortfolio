0b98e568917e51f4963195b41e72a53520db0f21
inunotaisho26 <ebfairweather@gmail.com>
2018-05-26 13:28:54 -0400

    adding Username field

--- AaronPortfolio/ClientApp/Interfaces/RegInterface.tsx
+++ AaronPortfolio/ClientApp/Interfaces/RegInterface.tsx
@@ -1,6 +1,7 @@
 export interface IRegFields {
     FirstName: string,
     LastName: string,
+    Username: string,
     Email: string,
     Password: string,
     PasswordConfirm: string,
--- AaronPortfolio/ClientApp/Models/Registration.Model.tsx
+++ AaronPortfolio/ClientApp/Models/Registration.Model.tsx
@@ -2,17 +2,20 @@ export class RegistrationModel {
     constructor(
         FirstName: string,
         LastName: string,
+        Username: string,
         Email:string,
         Password:string,
         PasswordConfirm: string){
             this.FirstName = FirstName;
             this.LastName = LastName;
+            this.Username = Username;
             this.Email = Email;
             this.Password = Password;
             this.PasswordConfirm = PasswordConfirm;
     }
     FirstName: string;
     LastName: string;
+    Username: string;
     Email:string;
     Password:string;
     PasswordConfirm: string;  
