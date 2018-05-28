2a835dfe7252b937c4e03a5d3e989f987eb8beba
inunotaisho26 <ebfairweather@gmail.com>
2018-05-28 19:32:36 -0400

    adding helpers

---
+++ AaronPortfolio/Server/Helpers/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace AaronPortfolio.Server.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ AaronPortfolio/Server/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+using Scheduling.AaronPortfolio.Server.ViewModels;
+
+namespace AaronPortfolio.Server.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserViewModel>();
+            CreateMap<UserViewModel, User>();
+        }
+    }
+}
