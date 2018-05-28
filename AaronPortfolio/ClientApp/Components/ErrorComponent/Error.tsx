61210219a0d6bfb1d2901b75c38185d8c228631a
inunotaisho26 <ebfairweather@gmail.com>
2018-05-27 20:55:52 -0400

    adding error page

---
+++ AaronPortfolio/ClientApp/Components/ErrorComponent/Error.tsx
@@ -0,0 +1,18 @@
+﻿import * as React from 'react';
+
+
+
+export  class WebError extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="main-content not-found">
+                    <i className="material-icons icn-error">error_outline</i>
+                    <h2>Page Not Found</h2>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default WebError;
