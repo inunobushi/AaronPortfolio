91f09ca0b88c57dc0b31b81485624dd7a41a2c42
inunotaisho26 <ebfairweather@gmail.com>
2018-05-31 16:09:12 -0400

    altering datacontext to mongodb

---
+++ AaronPortfolio/Server/Helpers/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Scheduling.AaronPortfolio.Server.Models;
+
+namespace Scheduling.AaronPortfolio.Server.Helpers
+{
+    public class DataContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public DataContext(IOptions<Settings> settings) 
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+            {
+                _database = client.GetDatabase(settings.Value.Database);
+            }
+        }
+
+        public IMongoCollection<UserSchema> Users
+        {
+            get{
+                return _database.GetCollection<UserSchema>("UserSchema");
+            }    
+        }
+
+
+    }
+
+
+
+        //public class DataContext : DbContext
+        //{
+        //    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        //    public MongoDB.Bson<UserSchema> Users { get; set; }
+        //}
+
+}
