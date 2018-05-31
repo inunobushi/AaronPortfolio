53c51062aad5479e73414f0108c694821a63290f
inunotaisho26 <ebfairweather@gmail.com>
2018-05-31 16:01:40 -0400

    changing namespace for viewmodels

--- AaronPortfolio/Server/Controllers/ContactController.cs
+++ AaronPortfolio/Server/Controllers/ContactController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AaronPortfolio.Server.ViewModels;
+using Scheduling.AaronPortfolio.Server.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
