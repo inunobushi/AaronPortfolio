df4245dc9600c2324a788a7820b58d95b8dabd99
inunotaisho26 <ebfairweather@gmail.com>
2018-05-31 16:04:38 -0400

    adding "scheduling" to automapperprofile helper

--- AaronPortfolio/Server/Helpers/AutoMapperProfile.cs
+++ AaronPortfolio/Server/Helpers/AutoMapperProfile.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using AutoMapper;
 using Scheduling.AaronPortfolio.Server.ViewModels;
+using Scheduling.AaronPortfolio.Server.Models;
 
-namespace AaronPortfolio.Server.Helpers
+namespace Scheduling.AaronPortfolio.Server.Helpers
 {
     public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
         {
-            CreateMap<User, UserViewModel>();
-            CreateMap<UserViewModel, User>();
+            CreateMap<UserSchema, UserViewModel>();
+            CreateMap<UserViewModel, UserSchema>();
         }
     }
 }
