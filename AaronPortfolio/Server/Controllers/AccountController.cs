cd1a127d0f45103c66db3df04620b066158002e2
inunotaisho26 <ebfairweather@gmail.com>
2018-05-31 16:08:35 -0400

    another datacontext problem

--- AaronPortfolio/Server/Controllers/AccountController.cs
+++ AaronPortfolio/Server/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Scheduling.AaronPortfolio.Server.ViewModels;
+using Scheduling.AaronPortfolio.Server.Models;
 using Microsoft.Extensions.Logging;
 using AutoMapper;
 using Microsoft.IdentityModel.Tokens;
@@ -9,8 +10,8 @@ using System.Text;
 using Microsoft.Extensions.Options;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using AaronPortfolio.Server.Middleware.Authentication;
-using AaronPortfolio.Server.Helpers;
+using Scheduling.AaronPortfolio.Server.Middleware.Authentication;
+using Scheduling.AaronPortfolio.Server.Helpers;
 using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -27,16 +28,18 @@ namespace AaronPortfolio.Server.Account.Controllers
         private IManageAccountService _manageAccountService;
         private IMapper _mapper;
         private readonly AppSettings _appSettings;
+        private readonly DataContext _context = null;
 
-        public UsersController(
+        public AccountController(
             IAuthenticationService authService,
             IRegistrationService registrationService,
             ICommonServices commonServices,
             IManageAccountService manageAccountService,
             IMapper mapper,
-
+            IOptions<Settings> settings,
             IOptions<AppSettings> appSettings)
         {
+            _context = new DataContext(settings);
             _authService = authService;
             _registrationService = registrationService;
             _commonServices = commonServices;
@@ -49,14 +52,78 @@ namespace AaronPortfolio.Server.Account.Controllers
         // POST api/values
         [AllowAnonymous]
         [HttpPost("register")]
-		public void Register([FromBody]UserViewModel user)
+		public void Register([FromBody]UserViewModel userDto)
 		{
-            var newUser  = _mapper.Map<>(user);
+            var newUser  = _mapper.Map<UserSchema>(userDto);
 
             try
             {
                 // save 
-                _authService.Create(user, userDto.Password);
+                _registrationService.Create(newUser, userDto.Password);
+
+                Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                BadRequest(ex.Message);
+            }
+		}
+
+        // POST api/values
+        [AllowAnonymous]
+		[HttpPost("login")]
+        public void Login([FromBody]LoginViewModel user, UserSchema mongo)
+        {
+            var loginUser = _authService.Authenticate(user.Username, user.Password);
+
+            if (user == null)
+            {
+                Unauthorized();
+            }
+
+            //var tokenHandler = new JwtSecurityTokenHandler();
+            //var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            //var tokenDescriptor = new SecurityTokenDescriptor
+            //{
+            //    Subject = new ClaimsIdentity(new Claim[]
+            //    {
+            //        new Claim(ClaimTypes.Name, mongo.Id.ToString())
+            //    }),
+            //    Expires = DateTime.UtcNow.AddDays(7),
+            //    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            //};
+            //var token = tokenHandler.CreateToken(tokenDescriptor);
+            //var tokenString = tokenHandler.WriteToken(token);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _commonServices.GetAll();
+            var userDtos = _mapper.Map<IList<UserViewModel>>(users);
+            return Ok(userDtos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var user = _commonServices.GetById(id);
+            var userDto = _mapper.Map<UserViewModel>(user);
+            return Ok(userDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody]UserViewModel userDto)
+        {
+            // map dto to entity and set id
+            var user = _mapper.Map<UserSchema>(userDto);
+            user.Id = id;
+
+            try
+            {
+                // save 
+                _manageAccountService.Update(user, userDto.Password);
                 return Ok();
             }
             catch (AppException ex)
@@ -64,31 +131,13 @@ namespace AaronPortfolio.Server.Account.Controllers
                 // return error message if there was an exception
                 return BadRequest(ex.Message);
             }
-		}
+        }
 
-        // POST api/values
-        [AllowAnonymous]
-		[HttpPost("login")]
-        public void Login([FromBody]LoginViewModel user)
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
         {
-            var loginUser = user.Authenticate(user.Username, user.Password);
-
-            if (user == null)
-                return Unauthorized();
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
+            _manageAccountService.Delete(id);
+            return Ok();
         }
     }
 }
