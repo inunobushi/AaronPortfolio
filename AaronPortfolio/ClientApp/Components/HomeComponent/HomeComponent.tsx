8def237c682bd6b3e8ae061b07cdcb5033abe263
inunotaisho26 <ebfairweather@gmail.com>
2018-06-07 18:35:45 -0400

    making css changing to home page only

--- AaronPortfolio/ClientApp/Components/HomeComponent/HomeComponent.tsx
+++ AaronPortfolio/ClientApp/Components/HomeComponent/HomeComponent.tsx
@@ -12,9 +12,20 @@ export class HomeComponent extends React.Component{
         super(props);
     }
 
+    // componentDidMount(){
+    //     const body = document.querySelector("body");
+    //     body.style.backgroundColor = '#000000';
+
+    // }
+    // componentWillUnmount(){
+    //     console.log("hello");
+    //     const body = document.querySelector("body");
+    //     body.style.backgroundColor = 'inital';       
+    // }
+
     render() {
         return(
-            <div className="app" id="main">
+            <div className="app home" id="main">
                 <NavbarComponent />
                 <div className="main-page-content main-page">
                     <div id="home">
@@ -25,11 +36,11 @@ export class HomeComponent extends React.Component{
                                         <div className="col-md-12 col-sm-12">
                                             <div className="home-text wow fadeIn text-center">
                                                 <h1 className="cd-headline clip is-full-width">
-                                                    I Am
-                                                    <span className="cd-words-wrapper" style={{ width: 266 + 'px', overflow: 'hidden' }}>
-                                                        <b className="is-hidden">Creative</b>
+                                                    I Am 
+                                                    <span className="cd-words-wrapper" style={{ width: 266 + 'px', overflow: 'hidden', marginLeft: '10px' }}>
+                                                        <b className="is-visible">Creative</b>
                                                         <b className="is-hidden">Honest</b>
-                                                        <b className="is-visible">Aaron Fairweather</b>
+                                                        <b className="is-hidden">Aaron Fairweather</b>
                                                     </span>
                                                 </h1>
                                                 <Link className="about-contact1" to="/Contact">Contact Me</Link>
