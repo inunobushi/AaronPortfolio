9157f8374077e5452055d204f85e844b97fd6357
inunotaisho26 <ebfairweather@gmail.com>
2018-06-09 19:34:02 -0400

    fixing body styling

--- AaronPortfolio/ClientApp/Components/HomeComponent/HomeComponent.tsx
+++ AaronPortfolio/ClientApp/Components/HomeComponent/HomeComponent.tsx
@@ -2,29 +2,30 @@ import * as React from 'react';
 import Particles from 'react-particles-js';
 
 import NavbarComponent from '../NavbarComponent/NavbarComponent';
+import { CopyRightComponent } from '../CopyrightComponent/CopyrightComponent';
 
 import { Link, NavLink } from 'react-router-dom';
 
 
-export class HomeComponent extends React.Component{
+export class HomeComponent extends React.Component {
 
-    constructor(props:any){
+    constructor(props: any) {
         super(props);
     }
 
-    // componentDidMount(){
-    //     const body = document.querySelector("body");
-    //     body.style.backgroundColor = '#000000';
+    componentDidMount() {
+        const body = document.querySelector("body");
+        body.style.backgroundColor = '#000000';
 
-    // }
-    // componentWillUnmount(){
-    //     console.log("hello");
-    //     const body = document.querySelector("body");
-    //     body.style.backgroundColor = 'inital';       
-    // }
+    }
+    componentWillUnmount() {
+        console.log("hello");
+        const body = document.querySelector("body");
+        body.style.backgroundColor = '';
+    }
 
     render() {
-        return(
+        return (
             <div className="app home" id="main">
                 <NavbarComponent />
                 <div className="main-page-content main-page">
@@ -36,7 +37,7 @@ export class HomeComponent extends React.Component{
                                         <div className="col-md-12 col-sm-12">
                                             <div className="home-text wow fadeIn text-center">
                                                 <h1 className="cd-headline clip is-full-width">
-                                                    I Am 
+                                                    I Am
                                                     <span className="cd-words-wrapper" style={{ width: 266 + 'px', overflow: 'hidden', marginLeft: '10px' }}>
                                                         <b className="is-visible">Creative</b>
                                                         <b className="is-hidden">Honest</b>
@@ -52,6 +53,7 @@ export class HomeComponent extends React.Component{
                         </div>{/*<!-- end home-content-main  -->*/}
                     </div>{/*<!-- end home  -->*/}
                 </div>
+                <CopyRightComponent />{/*<!-- end copyright-details -->*/}
             </div>
 
         )
