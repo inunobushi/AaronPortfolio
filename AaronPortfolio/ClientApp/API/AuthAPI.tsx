4f448ab01e618b7760cb4d2e256e78ebe979cd5f
inunotaisho26 <ebfairweather@gmail.com>
2018-06-09 19:34:25 -0400

    changing authapi route

--- AaronPortfolio/ClientApp/API/AuthAPI.tsx
+++ AaronPortfolio/ClientApp/API/AuthAPI.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Registration = (user) => {
-   return axios.post('api/account/register/', user, { headers: { 'Content-Type': 'application/json' } })
-}
+   return axios.post('api/register/register', user, { headers: { 'Content-Type': 'application/json' } }).then(res => {console.log(res)});
+   }
 
 export default {Registration};
