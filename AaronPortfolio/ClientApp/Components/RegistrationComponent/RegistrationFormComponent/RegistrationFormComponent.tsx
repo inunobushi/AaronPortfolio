9c2ca19400bdc46fe203c3944d5e92426e90199a
inunotaisho26 <ebfairweather@gmail.com>
2018-06-10 12:09:35 -0400

    adding unknown

--- AaronPortfolio/ClientApp/Components/RegistrationComponent/RegistrationFormComponent/RegistrationFormComponent.tsx
+++ AaronPortfolio/ClientApp/Components/RegistrationComponent/RegistrationFormComponent/RegistrationFormComponent.tsx
@@ -45,7 +45,7 @@ class RegistrationFormComponent extends React.Component<IRegFields, {}> {
   updatePasswordConfirm = (e) => this.props.updateFieldAuth(e, 'PasswordConfirm');
 
   register() {
-    
+
     const { FirstName, LastName, Username, Email, Password, PasswordConfirm } = this.props;
     const user: RegistrationModel = new RegistrationModel(FirstName, LastName, Username, Email, Password, PasswordConfirm);
     this.props.submitForm(user);
@@ -54,81 +54,81 @@ class RegistrationFormComponent extends React.Component<IRegFields, {}> {
 
   render() {
     return (
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="main-body">
-                <div className="body-inner">
-                  <div className="card bg-white">
-                    <div className="card-content">
-                      <div className="row about-row">
-                        <div className="col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 contact-col ">
-                          <form onSubmit={this.register} className="form-horizontal ng-pristine ng-valid">
-                            <fieldset>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="text" onChange={this.updateFirstName}
-                                    value={this.props.FirstName} className="form-control" placeholder="First Name" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="text" onChange={this.updateLastName}
-                                    value={this.props.LastName} className="form-control" placeholder="Last Name" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="text" onChange={this.updateUsername}
-                                    value={this.props.Username} className="form-control" placeholder="Username" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="text" onChange={this.updateEmail}
-                                    value={this.props.Email} className="form-control email" placeholder="Email" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="password" onChange={this.updatePassword}
-                                    value={this.props.Password} className="form-control" placeholder="Password" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="form-group">
-                                <div className="ui-input-group">
-                                  <input type="password" onChange={this.updatePasswordConfirm}
-                                    value={this.props.PasswordConfirm} className="form-control" placeholder="Password Confirmation" required />
-                                  <span className="input-bar"></span>
-                                </div>
-                              </div>
-                              <div className="spacer"></div>
-                              <div className="form-group checkbox-field">
-                                <label className="text-small">
-                                  <input type="checkbox" id="check_box" />
-                                  <span className="ion-ios-checkmark-empty22 custom-check"></span> By clicking on sign up, you agree to <a href="javascript:;"><i>terms</i></a> and <a href="javascript:;"><i>privacy policy</i></a></label>
-                              </div>
-                            </fieldset>
-                            <button className="btn btn-success text-center" type="button" onClick={this.register}>Sign Up</button>
-                          </form>
-                        </div>
-                      </div>
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="main-body">
+            <div className="body-inner">
+              <div className="card bg-white">
+                <div className="card-content">
+                  <div className="row about-row">
+                    <div className="col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 contact-col ">
+                      <form onSubmit={this.register} className="form-horizontal ng-pristine ng-valid">
+                        <fieldset>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="text" onChange={this.updateFirstName}
+                                value={this.props.FirstName} className="form-control" placeholder="First Name" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="text" onChange={this.updateLastName}
+                                value={this.props.LastName} className="form-control" placeholder="Last Name" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="text" onChange={this.updateUsername}
+                                value={this.props.Username} className="form-control" placeholder="Username" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="text" onChange={this.updateEmail}
+                                value={this.props.Email} className="form-control email" placeholder="Email" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="password" onChange={this.updatePassword}
+                                value={this.props.Password} className="form-control" placeholder="Password" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="form-group">
+                            <div className="ui-input-group">
+                              <input type="password" onChange={this.updatePasswordConfirm}
+                                value={this.props.PasswordConfirm} className="form-control" placeholder="Password Confirmation" required />
+                              <span className="input-bar"></span>
+                            </div>
+                          </div>
+                          <div className="spacer"></div>
+                          <div className="form-group checkbox-field">
+                            <label className="text-small">
+                              <input type="checkbox" id="check_box" />
+                              <span className="ion-ios-checkmark-empty22 custom-check"></span> By clicking on sign up, you agree to <a href="javascript:;"><i>terms</i></a> and <a href="javascript:;"><i>privacy policy</i></a></label>
+                          </div>
+                        </fieldset>
+                        <button className="btn btn-success text-center" type="button" onClick={this.register}>Sign Up</button>
+                      </form>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
     )
   }
 }
 
 const mapStateToProps = (store) => {
-console.log(store.auth);
+  console.log(store.auth);
   return {
     FirstName: store.auth.FirstName,
     LastName: store.auth.LastName,
