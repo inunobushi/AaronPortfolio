56a49f27f2f0bd28d0ca87aa855ce0537a7280d0
inunotaisho26 <ebfairweather@gmail.com>
2018-06-10 12:07:55 -0400

    creating reusable error component

---
+++ AaronPortfolio/ClientApp/Common/Results.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+
+
+class Result extends React.Component{
+    render() {
+        return (
+            <p id="results" className="search-results">
+                Some Results
+            </p>
+        );
+    }
+};
+
+export default Result;
