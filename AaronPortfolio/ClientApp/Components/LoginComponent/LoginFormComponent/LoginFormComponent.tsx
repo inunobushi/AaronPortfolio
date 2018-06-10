abd54d1806ff064352837842547f88dad724f2b7
inunotaisho26 <ebfairweather@gmail.com>
2018-06-10 12:06:36 -0400

    changing login to be dry

--- AaronPortfolio/ClientApp/Components/LoginComponent/LoginFormComponent/LoginFormComponent.tsx
+++ AaronPortfolio/ClientApp/Components/LoginComponent/LoginFormComponent/LoginFormComponent.tsx
@@ -5,9 +5,11 @@ import { connect } from 'react-redux';
 
 import login from '../../../Actions/LoginActions';
 import AuthService from '../../../Services/AuthService';
+import FormFieldComponent from "../../../Common/FormField";
 import { LoginModel }from '../../../Models/login.model';
 import { ILoginFields } from '../../../Interfaces/LoginInterface';
 import { UPDATE_FIELD_LOGIN, LOGIN_USER } from '../../../Constants/Constants';
+import formFields from '../../../Assets/i18n/en/FormFields';
 import { Button } from 'react-bootstrap';
 
 
@@ -21,9 +23,21 @@ export class LoginFormComponent extends React.Component <ILoginFields, {}>{
         this.updatePassword = this.updatePassword.bind(this);
     }
     private AuthService: AuthService = new AuthService();
+
+    createLoginFields = () => {
+        const fields = Object.keys(formFields.Fields.Login);
+        let result = {};
+        fields.forEach(field => {
+            const fieldVals = formFields.Fields.Login[field];
+            result[fieldVals.state] = (e) => this.props.updateFieldLogin(e, fieldVals.store)
+        })
+        return result;
+    }
     
-    updateEmail = (e) => this.props.updateFieldLogin(e, 'Email');
-    updatePassword = (e) => this.props.updateFieldLogin(e, 'Password');
+    createLoginFields().updateEmail()
+    // `${Login.Email.state}` = (e) => this.props.{}(e, 'Email');
+    // updateEmail = (e) => this.props.updateFieldLogin(e, 'Email');
+    // updatePassword = (e) => this.props.updateFieldLogin(e, 'Password');
 
     loginUser (e){
         e.preventDefault();
@@ -39,22 +53,23 @@ export class LoginFormComponent extends React.Component <ILoginFields, {}>{
                 <div className="col-md-6 col-md-offset-3 col-sm-10 col-sm-offset-1 contact-col ">
                     <form onSubmit={this.loginUser} className="form-horizontal ng-pristine ng-valid">
                         <fieldset>
-                            <div className="form-group">
+                            <FormFieldComponent/>
+                            {/* <div className="form-group">
                                 <div className="ui-input-group">
                                     <input type="text" onChange={this.updateEmail} 
                                     value={this.props.Email}
                                     className="form-control email" placeholder="Email" id="email" required/>
                                     <span className="input-bar"></span>
                                 </div>
-                            </div>
-                            <div className="form-group">
+                            </div> */}
+                            {/* <div className="form-group">
                                 <div className="ui-input-group">
                                     <input type="password" onChange={this.updatePassword} 
                                     value={this.props.Password}
                                     className="form-control" placeholder="Password" required/>
                                     <span className="input-bar"></span>
                                 </div>
-                            </div>
+                            </div> */}
                         </fieldset>
                         <div className="card-action no-border text-right"> <Button onClick={this.loginUser} className="color-primary">Sign in</Button> </div>
                         <span className="divider-h"></span><a href="#/page/forgot-password">Forgot your password?</a>
